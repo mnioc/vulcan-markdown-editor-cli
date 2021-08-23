5ced616e0cfdaea662cf6239879556f0f81acfa6
juqipeng <pengjuqi@163.com>
2021-08-23 10:19:24 +0800

    增加线上demo页

--- config/webpack.demoPage.config.js
+++ config/webpack.demoPage.config.js
@@ -1,17 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // 开发模式
-    entry: path.join(__dirname, '../demo/src/index.js'), // 项目入口，处理资源文件的依赖关系
+    mode: 'production',
+    entry: path.join(__dirname, '../demo/src/index.js'),
     output: {
-        path: path.join(__dirname, '../demo/src/'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, '../docs/'),
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
     },
     module: {
         rules: [
             {
-                // 使用 babel-loader 来编译处理 js 和 jsx 文件
                 test: /\.(js|jsx)$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
@@ -27,7 +29,6 @@ module.exports = {
                             // modifyVars: {
                             //     '@primary-color': '#d7000f'
                             // },
-
                             javascriptEnabled: true
                         }
                     }
@@ -35,20 +36,15 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: /\.min\.css$/,
-                loader: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.min\.css$/,
-                loader: ['style-loader', 'css-loader']
+                loader: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|jpg|jpeg|gif)/,
                 use: {
                     loader: 'url-loader',
                     options: {
-                        outputPath: 'images/', // 图片输出的路径
-                        limit: 10 * 1024 // <10kb 使用base64
+                        outputPath: 'images/',
+                        limit: 10 * 1024 //
                     }
                 }
             },
@@ -60,17 +56,19 @@ module.exports = {
             }
         ]
     },
-    devServer: {
-        contentBase: path.join(__dirname, '../demo/src/'),
-        compress: true,
-        host: '127.0.0.1',
-        port: 8080,
-        open: true
-    },
     plugins: [
+        new MiniCssExtractPlugin({}),
         new HtmlWebpackPlugin({
             template: './demo/src/index.html',
-            filename: 'index.html'
+            filename: 'index.html',
+            inject: true, // true：默认值，script标签位于html文件的 body 底部
+            hash: true, // 在打包的资源插入html会加上hash
+            //  html 文件进行压缩
+            minify: {
+                removeComments: true, //去注释
+                collapseWhitespace: true, //压缩空格
+                removeAttributeQuotes: true //去除属性引用
+            }
         })
     ]
 };
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,18 +2,17 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 用于将组件的css打包成单独的文件输出到`lib`目录中
 
 module.exports = {
-    mode: 'production', // 开发模式
+    mode: 'production',
     entry: path.join(__dirname, '../src/index.js'),
     output: {
-        path: path.join(__dirname, '../lib/'),
+        path: path.join(__dirname, '../docs/'),
         filename: 'index.js',
-        libraryTarget: 'umd', // 采用通用模块定义
-        libraryExport: 'default' // 兼容 ES6 的模块系统、CommonJS 和 AMD 模块规范
+        libraryTarget: 'umd',
+        libraryExport: 'default'
     },
     module: {
         rules: [
             {
-                // 使用 babel-loader 来编译处理 js 和 jsx 文件
                 test: /\.(js|jsx)$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
@@ -29,7 +28,6 @@ module.exports = {
                             // modifyVars: {
                             //     '@primary-color': '#d7000f'
                             // },
-
                             javascriptEnabled: true
                         }
                     }
@@ -44,8 +42,8 @@ module.exports = {
                 use: {
                     loader: 'url-loader',
                     options: {
-                        outputPath: 'images/', // 图片输出的路径
-                        limit: 10 * 1024 // <10kb 使用base64
+                        outputPath: 'images/',
+                        limit: 10 * 1024 //
                     }
                 }
             },
@@ -59,11 +57,10 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'main.min.css' // 提取后的css的文件名
+            filename: 'main.min.css'
         })
     ],
     externals: {
-        // 定义外部依赖，避免把react和react-dom打包进去
         react: {
             root: 'React',
             commonjs2: 'react',
