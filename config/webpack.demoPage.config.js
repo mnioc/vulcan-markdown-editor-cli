7127f995d2f9aeb5d41867f233fc57520a3afe2c
juqipeng <pengjuqi@163.com>
2021-08-25 10:03:38 +0800

    demo页

--- config/webpack.demoPage.config.js
+++ config/webpack.demoPage.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: path.join(__dirname, '../demo/src/index.js'),
     output: {
         path: path.join(__dirname, '../docs/'),
-        filename: 'index.js',
+        filename: 'bundle.js',
         libraryTarget: 'umd',
         libraryExport: 'default'
     },
