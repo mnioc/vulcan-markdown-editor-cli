e63803ebc9da94307998bf0c338815beeb7d27e7
juqipeng <pengjuqi@163.com>
2021-08-25 16:46:05 +0800

    Updates

--- src/component/AntdIcon.js
+++
@@ -1,3 +0,0 @@
-import Icon from '@ant-design/icons/lib/components/Icon';
-
-export default Icon;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import MDEditor from './component/mdEditor/MDEditor';
-import MDPriview from './component/mdPriview/index';
-
-const _ = {
-    MDEditor: MDEditor,
-    MDPriview: MDPriview
-};
-
-export default _;
